541699dda1bd964b7e22df9d24d2350a472209a9
Weestack <Alexander@helius.dk>
2021-03-10 14:09:07 +0100

    minor cleanup and added a few comments for tests.sh

--- tests.sh
+++ tests.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Clean and make
 make clean && make
+
+# Loop over each file
 for FILE in tests_programs/*
-do
-  echo "Testing File " $FILE;
-  cat $FILE | ./runoff
-done;
+    do
+        # echo out file name
+        echo "Testing File " $FILE;
+        # pass file into ./runoff
+        cat $FILE | ./runoff
+    done;
