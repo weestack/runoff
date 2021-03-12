0217750ed735434b49ddf7f938746c75fc4d4e0a
Peter Mikkelsen <petermikkelsen10@gmail.com>
2021-03-12 10:36:16 +0100

    Initial work on writing the mkNode functions

--- ast.c
+++ ast.c
@@ -2,153 +2,147 @@
 #include <stdlib.h>
 #include "ast.h"
 
+static AstNode *mkNode(int type) {
+	AstNode *node = malloc(sizeof(AstNode));
+	node->tag = type;
+	node->next = NULL;
+	return node;
+}
 
-
-AstNode *mkProgNode(){
+AstNode *mkProgNode(AstNode *toplevels){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Prog;
         node->next = NULL;
+        node->node.ProgNode.toplevels = toplevels;
         return node;
 }
 
-
-AstNode *mkDefineConstNode(){
+AstNode *mkDefineConstNode(AstNode *identifier, AstNode *int_literal){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = DefineConst;
         node->next = NULL;
+        node->node.DefineConstNode.identifier = identifier;
+        node->node.DefineConstNode.int_literal = int_literal;
         return node;
 }
 
-
-AstNode *mkDefineFunctionNode(){
+AstNode *mkDefineFunctionNode(AstNode *identifier, AstNode *parameters, AstNode *type, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = DefineFunction;
         node->next = NULL;
+        node->node.DefineFunctionNode.identifier = identifier;
+        node->node.DefineFunctionNode.parameters = parameters;
+        node->node.DefineFunctionNode.type = type;
+        node->node.DefineFunctionNode.statement = statement;
         return node;
 }
 
-
-AstNode *mkDefineTaskNode(){
+AstNode *mkDefineTaskNode(AstNode *identifier, AstNode *parameters, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = DefineTask;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkDefineMessageNode(){
+AstNode *mkDefineMessageNode(AstNode *messagesIdentifiers){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = DefineMessage;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkIncludeRunoffFileNode(){
+AstNode *mkIncludeRunoffFileNode(AstNode *identifier){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = IncludeRunoffFile;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkMessageIdentifierNode(){
+AstNode *mkMessageIdentifierNode(AstNode *identifier, AstNode *parameters){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = MessageIdentifier;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkStructMemberNode(){
+AstNode *mkStructMemberNode(AstNode *identifier, AstNode *type){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = StructMember;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkParameterNode(){
+AstNode *mkParameterNode(AstNode *type, AstNode *identifier){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Parameter;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkStructTypeNode(){
+AstNode *mkStructTypeNode(AstNode *identifier){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = StructType;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkArrayTypeNode(){
+AstNode *mkArrayTypeNode(AstNode *type, AstNode *int_literal){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = ArrayType;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkWhileNode(){
+AstNode *mkWhileNode(AstNode *expression, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = While;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkForNode(){
+AstNode *mkForNode(AstNode *expressionInit, AstNode *expressionTest, AstNode *expressionUpdate, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = For;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkSwitchNode(){
+AstNode *mkSwitchNode(AstNode *expression, AstNode *cases){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Switch;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkSwitchCaseNode(){
+AstNode *mkSwitchCaseNode(AstNode *literal, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = SwitchCase;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkIfNode(){
+AstNode *mkIfNode(AstNode *expression, AstNode *statements, AstNode *elsePart){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = If;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkElseIfNode(){
+AstNode *mkElseIfNode(AstNode *expression, AstNode *statements, AstNode *elsePart){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = ElseIf;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkElseNode(){
+AstNode *mkElseNode(AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Else;
         node->next = NULL;
         return node;
 }
 
-
-AstNode *mkReceiveNode(){
+AstNode *mkReceiveNode(AstNode *cases){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Receive;
         node->next = NULL;
@@ -156,7 +150,7 @@ AstNode *mkReceiveNode(){
 }
 
 
-AstNode *mkReceiveCaseNode(){
+AstNode *mkReceiveCaseNode(AstNode *messageName, AstNode *dataNames, AstNode *statements){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = ReceiveCase;
         node->next = NULL;
@@ -164,7 +158,7 @@ AstNode *mkReceiveCaseNode(){
 }
 
 
-AstNode *mkVarDeclNode(){
+AstNode *mkVarDeclNode(AstNode *identifier, AstNode *expression){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = VarDecl;
         node->next = NULL;
@@ -172,7 +166,7 @@ AstNode *mkVarDeclNode(){
 }
 
 
-AstNode *mkBinaryOperationNode(){
+AstNode *mkBinaryOperationNode(AstNode *expression_left, int operator, AstNode *expression_right){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = BinaryOperation;
         node->next = NULL;
@@ -180,7 +174,7 @@ AstNode *mkBinaryOperationNode(){
 }
 
 
-AstNode *mkVariableLocationNode(){
+AstNode *mkVariableLocationNode(AstNode *identifier){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = VariableLocation;
         node->next = NULL;
@@ -188,7 +182,7 @@ AstNode *mkVariableLocationNode(){
 }
 
 
-AstNode *mkStructLocationNode(){
+AstNode *mkStructLocationNode(AstNode *identifier, AstNode *location){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = StructLocation;
         node->next = NULL;
@@ -196,7 +190,7 @@ AstNode *mkStructLocationNode(){
 }
 
 
-AstNode *mkArrayLocationNode(){
+AstNode *mkArrayLocationNode(AstNode *identifier, AstNode *indicies){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = ArrayLocation;
         node->next = NULL;
@@ -204,7 +198,7 @@ AstNode *mkArrayLocationNode(){
 }
 
 
-AstNode *mkUnaryOperationNode(){
+AstNode *mkUnaryOperationNode(int operator, AstNode *expression){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = UnaryOperation;
         node->next = NULL;
@@ -212,7 +206,7 @@ AstNode *mkUnaryOperationNode(){
 }
 
 
-AstNode *mkFunctionCallNode(){
+AstNode *mkFunctionCallNode(AstNode *identifier, AstNode *arguments){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = FunctionCall;
         node->next = NULL;
@@ -220,7 +214,7 @@ AstNode *mkFunctionCallNode(){
 }
 
 
-AstNode *mkAssignmentNode(){
+AstNode *mkAssignmentNode(AstNode *location, AstNode *expression){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = Assignment;
         node->next = NULL;
@@ -228,7 +222,7 @@ AstNode *mkAssignmentNode(){
 }
 
 
-AstNode *mkTernaryOperatorNode(){
+AstNode *mkTernaryOperatorNode(AstNode *expressionTest, AstNode *expressionTrue, AstNode *expressionFalse){
         AstNode *node = malloc(sizeof(AstNode));
         node->tag = TernaryOperator;
         node->next = NULL;
