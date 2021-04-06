e61754474fa44f8931d93822e04152b23b2bb2a1
Peter Mikkelsen <petermikkelsen10@gmail.com>
2021-04-06 10:12:33 +0200

    Fix pptest

--- pptest.sh
+++ pptest.sh
@@ -7,9 +7,9 @@ tmpdir=$(mktemp -d)
 
 # Loop over each file
 for FILE in tests_programs/*
-    do
-    	file=$(basename $FILE)
-        ./runoff $FILE --prettyprint > $tmpdir/$file-1
-		./runoff $tmpdir/$file-1 --prettyprint > $tmpdir/$file-2
+	do
+		file=$(basename $FILE)
+		./runoff -p $FILE > $tmpdir/$file-1
+		./runoff -p $tmpdir/$file-1 > $tmpdir/$file-2
 		diff -Naur $tmpdir/$file-1 $tmpdir/$file-2
-    done;
+	done;
