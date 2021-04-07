a2a3608a4b53b226a66a5eadb090211985d75aba
Dovy <djasulaitis05@gmail.com>
2021-04-07 15:23:40 +0200

    Added some broken function generation and codegen.h

--- codegen.c
+++ codegen.c
@@ -6,41 +6,104 @@
 #include "ast.h"
 
 /* Tænker at hvis codegen returnere en stor streng fra heapen
-   lidt i stil med prettyprint, så er det smart, for så kan vi
-   selv bestemme om main() i runoff.c skal printe eller putte
-   over i en fil eller hvad vi nu vil gøre med strengen. */
+	lidt i stil med prettyprint, så er det smart, for så kan vi
+	selv bestemme om main() i runoff.c skal printe eller putte
+	over i en fil eller hvad vi nu vil gøre med strengen. */
 
-char *generateCode(char *code, AstNode *tree);
+char *generateNodeCode(char *code, AstNode *tree);
+void processBlock(char *code, AstNode *tree);
+char *getBuiltInTypeLiteral(int type);
 
-char *codegen(AstNode *tree){
+char *codegen(AstNode *tree) {
 	/* AstNode *children = getChildren(tree); */
 	char *code = malloc(1000000);
-
-	return (generateCode(code, getChildren(tree), nodeLength(tree)));
+	generateNodeCode(code, tree);
+	return code;
 }
 
-char *generateCode(char *code, AstNode *tree) {
-	int i;
-	AstNode currentNode = tree;
+void processBlock(char* code, AstNode *tree) {
+	AstNode *child;
+	for (child = tree; child != NULL; child = child->next) {
+		strcat(code, generateNodeCode(code, child));
+	}
+}
+
+char *generateNodeCode(char *code, AstNode *tree) {
 
 	switch (tree->tag) {
 		case Prog:
+			processBlock(code, tree->node.Prog.toplevels);
+			break;
 		case DefineFunction:
-		case DefineTask:
+			strcat(code, getBuiltInTypeLiteral(tree->node.DefineFunction.type->node.BuiltinType.type));
+			strcat(code, smprintf(" %s(", tree->node.DefineFunction.identifier->node.Identifier.symbol->name));
+			processBlock(code, tree->node.DefineFunction.parameters);
+			strcat(code, ") {");
+			
+			processBlock(code, tree->node.DefineFunction.statements);
+
+			strcat(code, "}");
+			
+
+			break;
+		case Parameter:
+			/*
+			strcat(code, smprintf("%s ", getBuiltInTypeLiteral(tree->node.Parameter.type->node.BuiltinType.type)));
+			strcat(code, tree->node.Parameter.identifier->node.Identifier.symbol->name);
+			if (tree->node.next != NULL) strcat(code, ", ");
+			*/
+			break;
 		default:
-			strcat(code, smprintf("//[unknown_error] tag: %d", tree->tag));
+			return "";
 	}
+
+	return code;
 }
-	if (currentNode->node->next != NULL) {
-		/* returns next in list */
-		return generateCode
-	} else if (nodeLength(getChildren(currentNode))) {
-		/* returns next level of tree */
-		return;
-	} else {
-		/* returns code string */
-		return code;
+
+char *getBuiltInTypeLiteral(int type) {
+	switch (type) {
+		case builtintype_uint8:
+			return "byte";
+			break;
+		case builtintype_uint16:
+			return "unsigned int";
+			break;
+		case builtintype_uint32:
+			return "unsigned long";
+			break;
+		case builtintype_uint64:
+			return "unsigned long";
+			break;
+		case builtintype_int8:
+			return "char";
+			break;
+		case builtintype_int16:
+			return "int";
+			break;
+		case builtintype_int32:
+			return "long";
+			break;
+		case builtintype_int64:
+			return "long";
+			break;
+		case builtintype_unknownInt:
+			return "int";
+			break;
+		case builtintype_float:
+			return "float";
+			break;
+		case builtintype_void:
+			return "void";
+			break;
+		case builtintype_bool:
+			return "boolean";
+			break;
+		case builtintype_msg:
+		case builtintype_taskid:
+		case builtintype_pinid:
+			return "int";
+			break;		
+		default:
+			return "unknownType";
 	}
-
-
 }
---
+++ codegen.h
@@ -0,0 +1,1 @@
+char *codegen(AstNode *tree);
