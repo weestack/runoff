c12d4412ef63a496430075286333f2cdfb2a2587
Dovy <djasulaitis05@gmail.com>
2021-04-07 16:53:07 +0200

    Function type, identifier and parameter codegen (also fixed nasty segfaults :()

--- codegen.c
+++ codegen.c
@@ -10,48 +10,73 @@
 	selv bestemme om main() i runoff.c skal printe eller putte
 	over i en fil eller hvad vi nu vil gøre med strengen. */
 
-char *generateNodeCode(char *code, AstNode *tree);
-void processBlock(char *code, AstNode *tree);
+char *processBlock(AstNode *node, char *sep, int end);
 char *getBuiltInTypeLiteral(int type);
 
-char *codegen(AstNode *tree) {
-	/* AstNode *children = getChildren(tree); */
-	char *code = malloc(1000000);
-	generateNodeCode(code, tree);
-	return code;
-}
+char *codegen(AstNode *tree) { 
+	char *id = NULL;
+	char *type = NULL;
+	char *params = NULL;
+	char *stmts = NULL;
 
-void processBlock(char* code, AstNode *tree) {
-	AstNode *child;
-	for (child = tree; child != NULL; child = child->next) {
-		strcat(code, generateNodeCode(code, child));
-	}
-}
+	char *result = NULL;
 
-char *generateNodeCode(char *code, AstNode *tree) {
+	if (tree == NULL) return smprintf("");
 
 	switch (tree->tag) {
 		case Prog:
-			processBlock(code, tree->node.Prog.toplevels);
+			result = processBlock(tree->node.Prog.toplevels, "\n", 0);
 			break;
 		case DefineFunction:
-			strcat(code, getBuiltInTypeLiteral(tree->node.DefineFunction.type->node.BuiltinType.type));
-			strcat(code, smprintf(" %s(", tree->node.DefineFunction.identifier->node.Identifier.symbol->name));
-			processBlock(code, tree->node.DefineFunction.parameters);
-			strcat(code, ") {");
-			processBlock(code, tree->node.DefineFunction.statements);
-			strcat(code, "}");
+			type = smprintf("%s", getBuiltInTypeLiteral(tree->node.DefineFunction.type->node.BuiltinType.type));
+			id = smprintf("%s", tree->node.DefineFunction.identifier->node.Identifier.symbol->name);
+			params = processBlock(tree->node.DefineFunction.parameters, ", ", 0);
+			stmts = processBlock(tree->node.DefineFunction.statements, ";", 1);
+			result = smprintf("%s %s(%s) {%s}", type, id, params, stmts);
 			break;
 		case Parameter:
-			strcat(code, smprintf("%s ", getBuiltInTypeLiteral(tree->node.Parameter.type->node.BuiltinType.type)));
-			strcat(code, tree->node.Parameter.identifier->node.Identifier.symbol->name);
-			if (tree->next != NULL) strcat(code, ", ");
+			type = smprintf("%s", getBuiltInTypeLiteral(tree->node.Parameter.type->node.BuiltinType.type));
+			id = smprintf("%s", tree->node.Parameter.identifier->node.Identifier.symbol->name);
+			result = smprintf("%s %s", type, id);
 			break;
 		default:
-			return "";
+			result = smprintf("");
 	}
 
-	return code;
+	free(id);
+	free(type);
+	free(params);
+	free(stmts);
+
+	return result;
+}
+/* prettyprinterlist copy :joyd: */
+char *processBlock(AstNode *node, char *sep, int end){
+	char *result;
+	char *prev;
+	char *childstr;
+	AstNode *child;
+
+	if(node == NULL)
+		return smprintf("");
+
+	child = node->next;
+	result = codegen(node);
+
+	while(child != NULL){
+		prev = result;
+		childstr = codegen(child);
+		result = smprintf("%s%s%s", prev, sep, childstr);
+		free(prev);
+		free(childstr);
+		child = child->next;
+	}
+	if(end){
+		prev = result;
+		result = smprintf("%s%s", prev, sep);
+		free(prev);
+	}
+	return result;
 }
 
 char *getBuiltInTypeLiteral(int type) {
