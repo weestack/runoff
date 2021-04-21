9d4d45ba00bd93e9beafce8d29fd99752cf603e8
Fony-Stark <kholt19@student.aau.dk>
2021-04-21 12:50:15 +0000

    triagle

--- arduino_helpers.ino
+++ arduino_helpers.ino
@@ -13,16 +13,16 @@ int runoff_createTask(TaskFunction_t taskFunction, char *taskName, int enumIdent
   BaseType_t xReturned;
 
    /* Create the task, storing the handle. */
-    xReturned = xTaskCreate(
-    taskFunction,       /* Function that implements the task. */
-    taskName,          /* Text name for the task. */
-    configMINIMAL_STACK_SIZE,              /* 196 * 8 * 2 = bits allocated to this task */
-    args,            /* Parameter passed into the task. */
-    10,             /* Priority at which the task is created. */
-    NULL );        /* Used to pass out the created task's handle. */
+  xReturned = xTaskCreate(
+  taskFunction,                            /* Function that implements the task. */
+  taskName,                               /* Text name for the task. */
+  configMINIMAL_STACK_SIZE,              /* 196 * 8 * 2 = bits allocated to this task */
+  args,                                 /* Parameter passed into the task. */
+10,                                    /* Priority at which the task is created. */
+  NULL );                             /* Used to pass out the created task's handle. */
 
-   if( xReturned != pdPASS ){
-    return NULL;
-   }
-   return taskId;
+  if( xReturned != pdPASS ){
+  return NULL;
+  }
+  return taskId;
 }
