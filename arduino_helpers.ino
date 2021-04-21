eecd210caf86ea539e9500baf1cb8d4b6168ec80
Weestack <Alexander@helius.dk>
2021-04-21 13:42:23 +0200

    made some basic settings for tasks

--- arduino_helpers.ino
+++ arduino_helpers.ino
@@ -3,28 +3,27 @@
 #define configUSE_TIME_SLICING 1
 #define messageQueueSize 3
 
-void setup() {  
+void setup() {
   /* remember to run users setup function! */
 }
 
 void loop(){}
 // End of helper file
-
-char *createTask(TaskFunction_t taskFunction, char *taskName, configSTACK_DEPTH_TYPE stackSize, void *args,){
+int runoff_createTask(TaskFunction_t taskFunction, char *taskName, int enumIdentifier, void *args){
   BaseType_t xReturned;
-  TaskHandle_t xHandle = NULL;
 
+    taskName = ("%s_%d", taskName, identifier);
    /* Create the task, storing the handle. */
     xReturned = xTaskCreate(
     taskFunction,       /* Function that implements the task. */
     taskName,          /* Text name for the task. */
-    stackSize,      /* Stack size in words, not bytes. */
-    args,    /* Parameter passed into the task. */
-    10,/* Priority at which the task is created. */
-    &xHandle );      /* Used to pass out the created task's handle. */
+    configMINIMAL_STACK_SIZE,              /* 196 * 8 * 2 = bits allocated to this task */
+    args,            /* Parameter passed into the task. */
+    10,             /* Priority at which the task is created. */
+    NULL );        /* Used to pass out the created task's handle. */
 
    if( xReturned != pdPASS ){
-    return null/* The task was created.  Use the task's handle to delete the task.  */
+    return NULL;
    }
-   return xHandle;
+   return taskId;
 }
