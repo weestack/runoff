18527225b05eeb2a979894d363f6dd8e360ccb10
Fony-Stark <kholt19@student.aau.dk>
2021-04-22 07:34:54 +0000

    I think the runoff_createTask function is done now :sunglasses:

--- arduino_helpers.ino
+++ arduino_helpers.ino
@@ -9,20 +9,22 @@ void setup() {
 
 void loop(){}
 // End of helper file
-int runoff_createTask(TaskFunction_t taskFunction, char *taskName, int enumIdentifier, void *args){
+int runoff_createTask(TaskFunction_t taskFunction, char *taskName, void *args){
   BaseType_t xReturned;
+  static int taskId = -1;
+  taskId++;
 
    /* Create the task, storing the handle. */
   xReturned = xTaskCreate(
-  taskFunction,                            /* Function that implements the task.          */
-  taskName,                               /* Text name for the task.                     */
-  configMINIMAL_STACK_SIZE,              /* 196 * 8 * 2 = bits allocated to this task   */
-  args,                                 /* Parameter passed into the task.             */
-10,                                    /* Priority at which the task is created.      */
-  NULL );                             /* Used to pass out the created task's handle. */
+  taskFunction,                            /* Function that implements the task.   *\           */
+  taskName,                               /* Text name for the task.                *\         */
+  configMINIMAL_STACK_SIZE,              /* 196 * 8 * 2 = bits allocated to this task*\       */
+  args,                                 /* Parameter passed into the task.            *\     */
+  10,                                  /* Priority at which the task is created.       *\   */
+  NULL );                             /* Used to pass out the created task's handle.    *\ */
 
   if( xReturned != pdPASS ){
-  return NULL;
+    return NULL;
   }
   return taskId;
 }
