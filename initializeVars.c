45866f2490f69a1f2bb181031de36e21e6cb5182
Fony-Stark <kholt19@student.aau.dk>
2021-05-05 08:36:34 +0000

    Added static, to all function and put all code in a top down depth first fashion. Also fixed minor bug with type being equal to null

--- initializeVars.c
+++ initializeVars.c
@@ -4,9 +4,20 @@
 #include "phases.h"
 
 static void insertInitCode(AstNode *);
+int isInitialized(InitializeInfo *, Type *);
 static int isInitializedArray(InitializeInfo *, Type *, AstNode *);
 static AstNode *getDefaultValue(Type *);
+static void insertArrayInitCode(InitializeInfo *, Type *, AstNode *, AstNode *, AstNode *, AstNode *);
+static AstNode *extendIndices(AstNode *, int);
+int canGetDefaultValue(InitializeInfo *, Type *);
 static void initializeStructFields(AstNode *, InitializeInfo *, AstNode *);
+static AstNode *makeStructLocation(AstNode *, AstNode *);
+static AstNode *copyIds(AstNode *);
+
+static void setInitializedArray(InitializeInfo *, Type *, AstNode *);
+void setInitializedStructField(InitializeInfo *, char *);
+
+/* Tree one */
 
 void initializeVars(AstNode *tree){
 	AstNode *chain = tree->chain;
@@ -22,40 +33,6 @@ void initializeVars(AstNode *tree){
 
 	if(chain != NULL)
 		initializeVars(chain);
-}
-
-static AstNode *extendIndices(AstNode *indices, int i){
-	AstNode *result = NULL;
-	AstNode *tmp;
-	for(tmp = indices; tmp != NULL; tmp = tmp->next){
-		AstNode *n = mkIntLiteralNode(tmp->node.IntLiteral.value);
-		result = append_node(result, n);
-	}
-	result = append_node(result, mkIntLiteralNode(i));
-	return result;
-}
-
-static void insertArrayInitCode(InitializeInfo *info, Type *type, AstNode *dims, AstNode *id, AstNode *indices, AstNode *decl){
-	if(dims != 0){
-		int i;
-		int size = dims->node.IntLiteral.value;
-		for(i = 0; i < size; i++){
-			if(!isInitializedArray(info->arrayInitialized[i], type, dims->next)){
-				AstNode *indicesNew;
-				indicesNew = extendIndices(indices, i);
-				insertArrayInitCode(info->arrayInitialized[i], type, dims->next, id, indicesNew, decl);
-			}
-		}
-	}else{
-		AstNode *loc = mkArrayLocationNode(id, indices);
-		AstNode *expr = getDefaultValue(type);
-		AstNode *assign = mkExprStmtNode(mkAssignmentNode(loc, expr));
-		assign->next = decl->next;
-		assign->chain = decl->chain;
-		assign->parent = decl->parent;
-		decl->next = assign;
-		decl->chain = assign;
-	}
 }
 
 static void insertInitCode(AstNode *exprstmt){
@@ -80,26 +57,117 @@ static void insertInitCode(AstNode *exprstmt){
 	}
 }
 
-AstNode *copyIds(AstNode *ids){
-	AstNode *newids = NULL;
-	AstNode *tmp;
-	for(tmp = ids; tmp != NULL; tmp = tmp->next){
-		AstNode *newid = mkIdentifierNode(tmp->node.Identifier.identifier);
-		newid->node.Identifier.symbol = tmp->node.Identifier .symbol;
-		newids = append_node(newids, newid);
+int isInitialized(InitializeInfo *info, Type *t){
+	if(t->tag == BuiltinTypeTag)
+		return info->varInitialized;
+	else if(t->tag == ArrayTypeTag)
+		return isInitializedArray(info,
+			t->tags.typeArray.elementType,
+			t->tags.typeArray.dimensions);
+	else if(t->tag == StructTypeTag){
+		StructInitializeInfo *sinfo = info->structInitialized;
+		for(; sinfo != NULL; sinfo = sinfo->next){
+			if(!isInitialized(sinfo->info, sinfo->symbol->type))
+				return 0;
+		}
+		return 1;
+	}else
+		return 0;
+}
+
+static int isInitializedArray(InitializeInfo *info, Type *t, AstNode *dims){
+	if(dims == NULL)
+		return isInitialized(info, t);
+	else{
+		int size = dims->node.IntLiteral.value;
+		int i;
+		for(i = 0; i < size; i++){
+			if(!isInitializedArray(info->arrayInitialized[i], t, dims->next))
+				return 0;
+		}
+		return 1;
 	}
-	return newids;
 }
 
-AstNode *makeStructLocation(AstNode *ids, AstNode *fieldloc){
-	ids = copyIds(ids);
-	if(ids == NULL)
-		return fieldloc;
-	else
-		return mkStructLocationNode(ids, makeStructLocation(ids->next, fieldloc));
+static AstNode *getDefaultValue(Type *type){
+	if(type->tag != BuiltinTypeTag)
+		return NULL;
+
+	switch(type->tags.typeBuiltin.builtinType){
+	case BuiltinTypeUint8:
+	case BuiltinTypeUint16:
+	case BuiltinTypeUint32:
+	case BuiltinTypeUint64:
+	case BuiltinTypeInt8:
+	case BuiltinTypeInt16:
+	case BuiltinTypeInt32:
+	case BuiltinTypeInt64:
+		return mkIntLiteralNode(0);
+	case BuiltinTypeFloat:
+		return mkFloatLiteralNode(0);
+	case BuiltinTypeBool:
+		return mkBoolLiteralNode(0);
+	default:
+		return NULL;
+	}
 }
 
-void initializeStructFields(AstNode *decl, InitializeInfo *info, AstNode *ids){
+
+static void insertArrayInitCode(InitializeInfo *info, Type *type, AstNode *dims, AstNode *id, AstNode *indices, AstNode *decl){
+	if(dims != 0){
+		int i;
+		int size = dims->node.IntLiteral.value;
+		for(i = 0; i < size; i++){
+			if(!isInitializedArray(info->arrayInitialized[i], type, dims->next)){
+				AstNode *indicesNew;
+				indicesNew = extendIndices(indices, i);
+				insertArrayInitCode(info->arrayInitialized[i], type, dims->next, id, indicesNew, decl);
+			}
+		}
+	}else{
+		AstNode *loc = mkArrayLocationNode(id, indices);
+		AstNode *expr = getDefaultValue(type);
+		AstNode *assign = mkExprStmtNode(mkAssignmentNode(loc, expr));
+		assign->next = decl->next;
+		assign->chain = decl->chain;
+		assign->parent = decl->parent;
+		decl->next = assign;
+		decl->chain = assign;
+	}
+}
+
+static AstNode *extendIndices(AstNode *indices, int i){
+	AstNode *result = NULL;
+	AstNode *tmp;
+	for(tmp = indices; tmp != NULL; tmp = tmp->next){
+		AstNode *n = mkIntLiteralNode(tmp->node.IntLiteral.value);
+		result = append_node(result, n);
+	}
+	result = append_node(result, mkIntLiteralNode(i));
+	return result;
+}
+
+int canGetDefaultValue(InitializeInfo *info, Type *t){
+	/* check if there exists a default value for type *t.
+	   If *t is a struct type, only check the uninitialized fields. */
+	if(t->tag == BuiltinTypeTag && getDefaultValue(t) != NULL)
+		return 1;
+	else if(t->tag == ArrayTypeTag && getDefaultValue(t->tags.typeArray.elementType) != NULL)
+		return 1;
+	else if(t->tag == StructTypeTag){
+		StructInitializeInfo *sinfo = info->structInitialized;
+		for(; sinfo != NULL; sinfo = sinfo->next){
+			InitializeInfo *fieldinfo = sinfo->info;
+			Type *fieldtype = sinfo->symbol->type;
+			if(!isInitialized(fieldinfo, fieldtype) && !canGetDefaultValue(fieldinfo, fieldtype))
+				return 0;
+		}
+		return 1;
+	}else
+		return 0;
+}
+
+static void initializeStructFields(AstNode *decl, InitializeInfo *info, AstNode *ids){
 	StructInitializeInfo *sinfo;
 
 	for(sinfo=info->structInitialized; sinfo != NULL; sinfo=sinfo->next){
@@ -127,62 +195,42 @@ void initializeStructFields(AstNode *decl, InitializeInfo *info, AstNode *ids){
 	}
 }
 
-static AstNode *getDefaultValue(Type *type){
-	if(type->tag != BuiltinTypeTag)
-		return NULL;
-
-	switch(type->tags.typeBuiltin.builtinType){
-	case BuiltinTypeUint8:
-	case BuiltinTypeUint16:
-	case BuiltinTypeUint32:
-	case BuiltinTypeUint64:
-	case BuiltinTypeInt8:
-	case BuiltinTypeInt16:
-	case BuiltinTypeInt32:
-	case BuiltinTypeInt64:
-		return mkIntLiteralNode(0);
-	case BuiltinTypeFloat:
-		return mkFloatLiteralNode(0);
-	case BuiltinTypeBool:
-		return mkBoolLiteralNode(0);
-	default:
-		return NULL;
-	}
+static AstNode *makeStructLocation(AstNode *ids, AstNode *fieldloc){
+	ids = copyIds(ids);
+	if(ids == NULL)
+		return fieldloc;
+	else
+		return mkStructLocationNode(ids, makeStructLocation(ids->next, fieldloc));
 }
 
-static int isInitializedArray(InitializeInfo *info, Type *t, AstNode *dims){
-	if(dims == NULL)
-		return isInitialized(info, t);
-	else{
-		int size = dims->node.IntLiteral.value;
-		int i;
-		for(i = 0; i < size; i++){
-			if(!isInitializedArray(info->arrayInitialized[i], t, dims->next))
-				return 0;
-		}
-		return 1;
+static AstNode *copyIds(AstNode *ids){
+	AstNode *newids = NULL;
+	AstNode *tmp;
+	for(tmp = ids; tmp != NULL; tmp = tmp->next){
+		AstNode *newid = mkIdentifierNode(tmp->node.Identifier.identifier);
+		newid->node.Identifier.symbol = tmp->node.Identifier .symbol;
+		newids = append_node(newids, newid);
 	}
+	return newids;
 }
 
-int isInitialized(InitializeInfo *info, Type *t){
+/* Tree two */
+void setInitialized(InitializeInfo *info, Type *t){
+	if(t == NULL){
+		return;
+	}
 	if(t->tag == BuiltinTypeTag)
-		return info->varInitialized;
+		info->varInitialized = 1;
 	else if(t->tag == ArrayTypeTag)
-		return isInitializedArray(info,
-			t->tags.typeArray.elementType,
-			t->tags.typeArray.dimensions);
+		setInitializedArray(info, t->tags.typeArray.elementType, t->tags.typeArray.dimensions);
 	else if(t->tag == StructTypeTag){
-		StructInitializeInfo *sinfo = info->structInitialized;
-		for(; sinfo != NULL; sinfo = sinfo->next){
-			if(!isInitialized(sinfo->info, sinfo->symbol->type))
-				return 0;
-		}
-		return 1;
-	}else
-		return 0;
+		Symbol *field;
+		for(field = t->tags.typeStruct.fields->symbols; field != NULL; field = field->next)
+			setInitializedStructField(info, field->name);
+	}
 }
 
-void setInitializedArray(InitializeInfo *info, Type *t, AstNode *dims){
+static void setInitializedArray(InitializeInfo *info, Type *t, AstNode *dims){
 	if(dims == NULL)
 		setInitialized(info, t);
 	else{
@@ -199,36 +247,4 @@ void setInitializedStructField(InitializeInfo *info, char *name){
 		if(strcmp(sinfo->symbol->name, name) == 0)
 			setInitialized(sinfo->info, sinfo->symbol->type);
 	}
-}
-
-void setInitialized(InitializeInfo *info, Type *t){
-	if(t->tag == BuiltinTypeTag)
-		info->varInitialized = 1;
-	else if(t->tag == ArrayTypeTag)
-		setInitializedArray(info, t->tags.typeArray.elementType, t->tags.typeArray.dimensions);
-	else if(t->tag == StructTypeTag){
-		Symbol *field;
-		for(field = t->tags.typeStruct.fields->symbols; field != NULL; field = field->next)
-			setInitializedStructField(info, field->name);
-	}
-}
-
-int canGetDefaultValue(InitializeInfo *info, Type *t){
-	/* check if there exists a default value for type *t.
-	   If *t is a struct type, only check the uninitialized fields. */
-	if(t->tag == BuiltinTypeTag && getDefaultValue(t) != NULL)
-		return 1;
-	else if(t->tag == ArrayTypeTag && getDefaultValue(t->tags.typeArray.elementType) != NULL)
-		return 1;
-	else if(t->tag == StructTypeTag){
-		StructInitializeInfo *sinfo = info->structInitialized;
-		for(; sinfo != NULL; sinfo = sinfo->next){
-			InitializeInfo *fieldinfo = sinfo->info;
-			Type *fieldtype = sinfo->symbol->type;
-			if(!isInitialized(fieldinfo, fieldtype) && !canGetDefaultValue(fieldinfo, fieldtype))
-				return 0;
-		}
-		return 1;
-	}else
-		return 0;
 }
